3015ab15e1b41e99c2f47c54eec3264a7db21349
Nguyen The Duc <nguyenduc91ndc@gmail.com>
2026-01-10 02:00:21 +0700

    Fix vite config for ESM

--- lucky-wheel/vite.config.ts
+++ lucky-wheel/vite.config.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -21,7 +22,7 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), '.'),
       }
     }
   };
