3bdd9dee4c4d276718010c75c58e9bb6158a95bb
Nguyen The Duc <nguyenduc91ndc@gmail.com>
2026-03-15 20:41:09 +0700

    Thêm app NhanXetTT27 vao public folder

--- APPNHANXETTUDONGtt27/TT27_NhanXet_App/vite.config.js
+++ APPNHANXETTUDONGtt27/TT27_NhanXet_App/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/nhan-xet-tt27/',
   define: {
     global: 'globalThis',
     'process.env': {},
   },
   build: {
     target: 'es2020',
+    outDir: '../../public/nhan-xet-tt27',
+    emptyOutDir: true,
     commonjsOptions: { transformMixedEsModules: true },
   },
   optimizeDeps: {
